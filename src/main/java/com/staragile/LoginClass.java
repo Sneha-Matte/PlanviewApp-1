333612c3e10f2ffe33f3b3ddfc2b93435411d115
Sneha-Matte <128369023+Sneha-Matte@users.noreply.github.com>
2023-03-25 10:04:11 +0530

    changes in class
    changes in repo

--- src/main/java/com/staragile/LoginClass.java
+++ src/main/java/com/staragile/LoginClass.java
@@ -4,7 +4,7 @@ public class LoginClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Dynamic project New Project Anuj Sharma");
+		System.out.println("Dynamic project New java Project Anuj Sharma");
 	}
 	public static void add() {
 		System.out.println("Performing Addition Operation !!");
